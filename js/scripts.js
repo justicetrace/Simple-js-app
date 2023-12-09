63e38e729591c990599f0b620f9b9231daedaef3
justicetrace <justicerivera33@gmail.com>
2023-12-08 21:40:52 -0500

    Update scripts.js
    -updated col 2-col 14
    -added loops for Pikachu, Umbreon, and Gallade.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 let pokemonList = [
-    {name: 'Pikachu', height: 0.4, types: ['electric'] },
-    {name: 'Umbreon', height: 1, types: ['dark'] },
-    {name: 'Gallade', height: 1.6, types: ['psychic','fighting'] },
-];
+    {name: 'Pikachu',
+     height: 0.4,
+     types: ['electric'] 
+    },
+    {name: 'Umbreon',
+     height: 1,
+     types: ['dark']
+    },
+    {name: 'Gallade',
+     height: 1.6,
+     types: ['psychic','fighting'] 
+    },
+  ];
+
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height < 1.7 && pokemonList[i].height > 0.9) {
+        document.write(pokemonList[i].name + ' is a big pokemon.');
+    } else if (pokemonList[i].height < 1.2) {
+        document.write(pokemonList[i].name + ' is a average pokemon.');
+    } else { document.write(pokemonList[i].name + ' is a small pokemon.');
+           }
+        }
