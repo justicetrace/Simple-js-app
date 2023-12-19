f7af6fd8dfe169e1ea3e0cdf98a9915479b6ab75
justicetrace <justicerivera33@gmail.com>
2023-12-19 16:49:23 -0500

    Updated script.js
    -- updated col 1-13
    -- added col 13-33
    --

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,33 @@
-let pokemonList = [
-  { name: "Pikachu", height: 0.4, types: ["electric"] },
-  { name: "Umbreon", height: 1, types: ["dark"] },
-  { name: "Gallade", height: 1.6, types: ["psychic", "fighting"] },
-];
-
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height < 1.7 && pokemonList[i].height > 0.9) {
-    document.write("Whoa, " + pokemonList[i].name + " is a big pokemon.");
-  } else if (pokemonList[i].height > 1.2) {
-    document.write("Wow, " + pokemonList[i].name + " is a average pokemon.");
-  } else {
-    document.write("Aw, " + pokemonList[i].name + " is a small pokemon.");
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Pikachu", height: 0.4, types: ["electric"] },
+    { name: "Umbreon", height: 1, types: ["dark"] },
+    { name: "Gallade", height: 1.6, types: ["psychic", "fighting"] },
+  ];
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-}
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Ditto" });
+console.log(pokemonRepository.getAll());
+
+// for (let i = 0; i < pokemonList.length; i++) {
+//   if (pokemonList[i].height < 1.7 && pokemonList[i].height > 0.9) {
+//     document.write("Whoa, " + pokemonList[i].name + " is a big pokemon.");
+//   } else if (pokemonList[i].height > 1.2) {
+//     document.write("Wow, " + pokemonList[i].name + " is a average pokemon.");
+//   } else {
+//     document.write("Aw, " + pokemonList[i].name + " is a small pokemon.");
+//   }
+// }
+pokemonList.forEach(function (item) {
+  document.write(" " + item.name + " is a cool pokemon.");
+});
